52afb36b65a446e8b73c060420dfa7d9b1503637
Sunil Jamod <sunilgj@infusionanalysts.com>
2021-06-26 13:16:45 +0530

    Remove unneccessary component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,24 @@
-import MessageListItem from '../components/MessageListItem';
-import { useState } from 'react';
-import { Message, getMessages } from '../data/messages';
 import {
-  IonBackButton,
-  IonThumbnail,
   IonButton,
   IonButtons,
   IonContent,
-  IonHeader,
-  IonList,
-  IonPage,
-  IonRefresher,
-  IonRefresherContent,
-  IonTitle,
-  IonToolbar,
-  useIonViewWillEnter,
-  IonIcon,
-  IonImg
-} from '@ionic/react';
-import { personCircle, close, star, ellipsisHorizontal, ellipsisVertical } from 'ionicons/icons';
-
-import './Home.css';
-import styled from 'styled-components'
+  IonHeader, IonIcon,
+  IonImg, IonPage, IonThumbnail, IonToolbar
+} from "@ionic/react";
+import {
+  close
+} from "ionicons/icons";
+import styled from "styled-components";
+import "./Home.css";
 
 const Title = styled.h1`
-font-size: 1.5em;
-text-align: center;
-color: palevioletred;
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
 `;
 
 const Home: React.FC = () => {
-
-  const [messages, setMessages] = useState<Message[]>([]);
-
   const logo: string = "http://placekitten.com/g/200/300";
-
-  useIonViewWillEnter(() => {
-    const msgs = getMessages();
-    setMessages(msgs);
-  });
-
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
 
   return (
     <IonPage id="home-page">
@@ -65,21 +39,7 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
-          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
-        </IonList>
+        <Title>Testing</Title>
       </IonContent>
     </IonPage>
   );
